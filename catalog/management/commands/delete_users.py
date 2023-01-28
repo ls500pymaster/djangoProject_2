c24447b011375e7ea8b4625d75c51a94cd8c0b21
pythonmaster <theone7877@gmail.com>
2023-01-28 20:22:11 +0200

    refactoring

--- catalog/management/commands/delete_users.py
+++ catalog/management/commands/delete_users.py
@@ -16,26 +16,3 @@ class Command(BaseCommand):
 			self.stdout.write(f"Can't delete superusers {users_ids}.")
 		else:
 			User.objects.filter(id__in=users_ids, is_superuser=False).delete()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		# users_ids = kwargs['user_id']
-		# try:
-		# 	for user in users_ids:
-		# 		if User.objects.filter(pk=user, is_superuser=True).exists():
-		# 			self.stdout.write(u'You can\'t delete superuser!')
-		# 		else:
-		# 			user = User.objects.get(pk=user).delete()
-		# except User.DoesNotExist:
-		# 	self.stdout.write(u'Does not exist!')
