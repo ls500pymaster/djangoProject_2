f7bfd821f7dbcc4019c98007df342bef18355e0b
pythonmaster <theone7877@gmail.com>
2023-01-28 20:21:31 +0200

    refactoring. deleted mydata.json

--- README.md
+++ README.md
@@ -1,18 +1,11 @@
-## Setup
-Please use .env file to add your SECRET_KEY
-If you use Pycharm, go to **Tools > Terminal > Environment variables**
-add name and value of your secret key. 
+## CUSTOM COMMANDS
 
+# Create fake user python
 
+python manage.py create_users 5 
 
-## Features
+# Deleter users (except superuser)
 
-Django 4.1 supports Python 3.8, 3.9, 3.10, and 3.11 (as of 4.1.3). We highly recommend and only officially support the latest release of each series.
+python manage.py delete_users 1 2 3 4 5
+      
 
-## What's new in Django 4.1
-
-- Asynchronous handlers for class-based views
-- Asynchronous ORM interface
-- Validation of Constraints
-- Form rendering accessibility
-- CSRF_COOKIE_MASKED setting
--- catalog/management/commands/delete_users.py
+++ catalog/management/commands/delete_users.py
@@ -6,15 +6,36 @@ class Command(BaseCommand):
 	help = u'Delete user'
 
 	def add_arguments(self, parser):
-		parser.add_argument('user_id', nargs='+', type=int, help='User ID')
+		parser.add_argument('user_id', nargs='+', type=int, help='Users ID')
+
+# Builtin lookup https://docs.djangoproject.com/en/4.1/ref/models/lookups/
 
 	def handle(self, *args, **kwargs):
 		users_ids = kwargs['user_id']
-		try:
-			for user in users_ids:
-				if User.objects.filter(pk=user, is_superuser=True).exists():
-					self.stdout.write(u'You can\'t delete superuser!')
-				else:
-					user = User.objects.get(pk=user).delete()
-		except User.DoesNotExist:
-			self.stdout.write(u'Does not exist!')
+		if User.objects.filter(id__in=users_ids, is_superuser=True):
+			self.stdout.write(f"Can't delete superusers {users_ids}.")
+		else:
+			User.objects.filter(id__in=users_ids, is_superuser=False).delete()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		# users_ids = kwargs['user_id']
+		# try:
+		# 	for user in users_ids:
+		# 		if User.objects.filter(pk=user, is_superuser=True).exists():
+		# 			self.stdout.write(u'You can\'t delete superuser!')
+		# 		else:
+		# 			user = User.objects.get(pk=user).delete()
+		# except User.DoesNotExist:
+		# 	self.stdout.write(u'Does not exist!')
