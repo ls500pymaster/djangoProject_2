8faec0117b71ece35450227bcd498a9f85c3bee0
pythonmaster <theone7877@gmail.com>
2023-02-07 16:28:37 +0200

    Middleware, ModelAdmin

--- djangoProject_2/settings.py
+++ djangoProject_2/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
 	'django.contrib.messages.middleware.MessageMiddleware',
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'catalog.middleware.custom_middleware.RequestLogMiddleware',
 ]
 
 ROOT_URLCONF = 'djangoProject_2.urls'
