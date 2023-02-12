71d30f4373b6afc18edfef3e163dd2ccc418f552
pythonmaster <theone7877@gmail.com>
2023-02-12 21:48:39 +0200

    annotate, aggregate

--- crm/urls.py
+++ crm/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path('authors/', views.authors_all, name='authors_all'),
     path('authors/<pk>/', views.author_solo, name='author_solo'),
     path('publishers/', views.publishers_all, name='publishers_all'),
-    path('publishers/<pk>', views.publisher_solo, name='publisher_solo'),
+    path('publishers/<pk>/', views.publisher_solo, name='publisher_solo'),
     path('books/', views.books_all, name='books_all'),
     path('books/<book_id>/', views.book_solo, name='authors'),
     path('stores/', views.stores_all, name='stores_all'),
-    path('stores/<pk>', views.store_solo, name='store_solo')
+    path('stores/<pk>/', views.store_solo, name='store_solo')
 ]
